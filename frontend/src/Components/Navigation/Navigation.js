70b9ebb470e3f32485b6865241e7118898871528
Nahush Patil <patilnahush41@gmail.com>
2024-07-17 21:02:28 +0530

    update navugation

--- frontend/src/Components/Navigation/Navigation.js
+++ frontend/src/Components/Navigation/Navigation.js
@@ -11,8 +11,7 @@ function Navigation({active, setActive}) {
             <div className="user-con">
                 <img src={avatar} alt="" />
                 <div className="text">
-                    <h2>Mike</h2>
-                    <p>Your Money</p>
+                    <h2>Jack</h2>
                 </div>
             </div>
             <ul className="menu-items">
